d06373e5fe4735091422d395abf9161729b36ce0
contigen <69436315+contigen@users.noreply.github.com.>
2026-01-26 08:03:49 +0100

    Add phantom wallet connection

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,20 +1,37 @@
-import type { MetaMaskInpageProvider } from '@metamask/providers'
-import { ethers } from 'ethers'
-
 declare global {
   interface Window {
-    ethereum?: MetaMaskInpageProvider
+    phantom?: {
+      solana: {
+        isPhantom?: boolean
+        connect(): Promise<{ publicKey: { toString(): string } }>
+        disconnect(): Promise<void>
+        signMessage(
+          message: Uint8Array,
+          display?: string,
+        ): Promise<{ signature: Uint8Array }>
+      }
+    }
   }
 }
 
+function getProvider() {
+  if ('phantom' in window) {
+    const provider = window.phantom?.solana
+
+    if (provider?.isPhantom) {
+      return provider
+    }
+  }
+  window.open('https://phantom.app/', '_blank')
+}
+
 export const connectWallet = async () => {
-  if (typeof window?.ethereum !== 'undefined') {
+  if (typeof window?.phantom !== 'undefined') {
     try {
-      await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const provider = new ethers.BrowserProvider(window.ethereum)
-      const signer = await provider.getSigner()
-      const address = await signer.getAddress()
-      return { provider, signer, address }
+      const provider = getProvider()
+      if (!provider) return ''
+      const resp = await provider.connect()
+      return { address: resp.publicKey.toString() }
     } catch {
       return ''
     }
