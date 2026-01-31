26e085c298437ddf1273fdf8f0e5311c02ea0f0a
contigen <69436315+contigen@users.noreply.github.com.>
2026-02-01 00:09:41 +0100

    Update output files deps

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,17 @@ const nextConfig: NextConfig = {
     '/**': [
       './node_modules/snarkjs/**',
       './node_modules/r1csfile/**',
-      './node_modules/@iden3/binfileutils/**',
+      './node_modules/@iden3/**',
       './node_modules/ffjavascript/**',
       './node_modules/fastfile/**',
       './node_modules/circom_runtime/**',
       './node_modules/wasmcurves/**',
       './node_modules/wasmbuilder/**',
+      './node_modules/web-worker/**',
+      './node_modules/bfj/**',
+      './node_modules/logplease/**',
+      './node_modules/@noble/**',
+      './node_modules/ejs/**',
     ],
   },
 };
