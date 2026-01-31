2f9b8e72be80c039083822789e4e01b9ca113f36
contigen <69436315+contigen@users.noreply.github.com.>
2026-01-31 18:08:54 +0100

    Refactor next.config.ts to configure webpack externals and output file tracing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  },
+  outputFileTracingIncludes: {
+    '/api/**': ['node_modules/snarkjs/**/*'],
+  },
 };
 
 export default nextConfig;
